610d8f3db0f2d207bb8df2148c6c2a1ebe3d6e5a
Deniz <auo@tuta.io>
2025-12-27 15:15:33 +0100

    served index.html

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import fs from "node:fs/promises"
 import http from "node:http"
 
 import { serveStatic } from "./utils/serveStatic.js"
@@ -10,11 +11,17 @@ const __dirname = import.meta.dirname
 // console.log(__dirname, "hello")
 
 
-const server = http.createServer((req, res) => {
-    serveStatic("yum yum", __dirname)
+const server = http.createServer(async (req, res) => {
+    const pathToResource = serveStatic(__dirname)
+    console.log("yayay", pathToResource)
+
+    const content = await fs.readFile(pathToResource, "utf8")
+    console.log(content)
+
+
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html")
-    res.end("<html><h1>Up and running!</h1></html>")
+    res.end(content)
 })
 
-server.listen(PORT, console.log(`Server on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Server on port: ${PORT}`))
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -3,5 +3,7 @@ import path from "node:path"
 export function serveStatic(baseDir) {
     const filePath = path.join(baseDir, "public", "index.html")
 
-    console.log("serve static", filePath)
+    //console.log("serve static", filePath)
+
+    return filePath
 }
