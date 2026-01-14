42393a37b54c1481cf2c468575b9e36c14014854
Deniz <auo@tuta.io>
2026-01-14 15:45:42 +0100

    minor change on error  catch.

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -3,6 +3,7 @@ import path from "node:path";
 
 import { sendResponse } from "./sendResponse.js";
 import { getContentType } from "./getContentType.js";
+import { send } from "node:process";
 
 export async function serveStatic(req, res, baseDir) {
 	const publicDir = path.join(baseDir, "public");
@@ -20,6 +21,17 @@ export async function serveStatic(req, res, baseDir) {
 
 		sendResponse(res, 200, contentType, content);
 	} catch (error) {
-		console.log(error);
+		console.log(error.code);
+
+		if(error.code === "ENOENT") {
+
+			const pathTo404 = path.join(publicDir, "404.html")
+			const content404 = await fs.readFile(pathTo404)
+	
+			sendResponse(res, 404, "text/html", content404)
+		} else {
+			sendResponse(res, 500, "text/html", `<html><h1>Server Error: ${error.code}</h1></html>`)
+		}
+
 	}
 }
