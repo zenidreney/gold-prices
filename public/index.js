8ea56e9d232f2f64db595a33037a62e37a86dbfb
Deniz <auo@tuta.io>
2026-01-15 18:06:05 +0100

    added disconnected logic.

--- public/index.js
+++ public/index.js
@@ -1,13 +1,54 @@
-async function fetchGoldPrice() {
-	const res = await fetch("/api/prices");
-	const data = await res.json();
-    console.log(data)
+const priceDisplaySpan = document.getElementById("price-display");
+const connectionStatusPara = document.getElementById("connection-status")
+const investBtn = document.getElementById("invest-btn")
 
-	const priceDisplaySpan = document.getElementById("price-display");
+let pollInterval = null
 
-	priceDisplaySpan.textContent = data.price;
+function startPolling() {
+	if (pollInterval === null) {
+		pollInterval = setInterval(fetchGoldPrice, 2500);
+	}
 }
 
-setInterval(fetchGoldPrice, 2500);
+function stopPolling() {
+	clearInterval(pollInterval);
+	pollInterval = null;
+}
+
+
+
+async function fetchGoldPrice() {
+
+
+	try {
+		const res = await fetch("/api/prices");
+
+		if(!res.ok) {
+			throw new Error("Server error")
+		}
+
+		const data = await res.json();
+	    //console.log(data)
+		priceDisplaySpan.textContent = data.price;
+		connectionStatusPara.textContent = "Live Price 🟢"
+		investBtn.disabled = false
+
+		startPolling()
+
+	} catch (error) {
+		console.log(error)
+		connectionStatusPara.textContent = "Disconnected 🔴"
+		priceDisplaySpan.textContent = "----.--"
+		investBtn.disabled = true
+
+		stopPolling()
+		setTimeout(fetchGoldPrice, 5000)
+	}
+
+
+}
+
 
 fetchGoldPrice();
+
+investBtn.addEventListener("click", () => console.log("investe button clicked"))
